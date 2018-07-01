a6d9587015bbc98bae72869c394b373d25e6300c
Pan Cheng <pchengeducation@hotmail.com>
2018-07-01 22:20:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-
+if word.scan(/\w+a|e|i|o|u/)
+  return true
+elsif !word.scan(/\w+a|e|i|o|u/)
+  return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
